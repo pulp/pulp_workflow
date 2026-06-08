5c682d6a09566556f5a3b4e6a5722bdd11ff6669
David Davis <daviddavis@users.noreply.github.com>
2026-06-08 13:36:10 -0400

    Fix version strings for 0.1.0 release

--- pulp_workflow/app/__init__.py
+++ pulp_workflow/app/__init__.py
@@ -6,7 +6,7 @@ class PulpWorkflowPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_workflow.app"
     label = "workflow"
-    version = "0.1.0.dev"
+    version = "0.1.0"
     python_package_name = "pulp-workflow"
     domain_compatible = True
 
