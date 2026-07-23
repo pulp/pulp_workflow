722820b9759d1183b00d93a015dd5c53fec94545
pulpbot <pulp-infra@redhat.com>
2026-07-23 18:02:16 +0000

    Bump version: 0.3.0 → 0.3.1.dev

--- pulp_workflow/app/__init__.py
+++ pulp_workflow/app/__init__.py
@@ -6,7 +6,7 @@ class PulpWorkflowPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_workflow.app"
     label = "workflow"
-    version = "0.3.0"
+    version = "0.3.1.dev"
     python_package_name = "pulp-workflow"
     domain_compatible = True
 
