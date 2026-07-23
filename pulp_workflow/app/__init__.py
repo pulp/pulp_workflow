2529660f951f704a134263b03596d6e087f9767c
pulpbot <pulp-infra@redhat.com>
2026-07-23 18:02:16 +0000

    Release 0.3.0

--- pulp_workflow/app/__init__.py
+++ pulp_workflow/app/__init__.py
@@ -6,7 +6,7 @@ class PulpWorkflowPluginAppConfig(PulpPluginAppConfig):
 
     name = "pulp_workflow.app"
     label = "workflow"
-    version = "0.3.0.dev"
+    version = "0.3.0"
     python_package_name = "pulp-workflow"
     domain_compatible = True
 
